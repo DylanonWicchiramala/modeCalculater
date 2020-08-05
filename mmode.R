d9eefbc1da71aa4a87a1e02eaa6a16080c049aa9
DylanonWicchiramala <43574914+DylanonWicchiramala@users.noreply.github.com>
2020-08-05 07:43:29 +0700

    Update mmode.R

--- mmode.R
+++ mmode.R
@@ -2,7 +2,7 @@ mmode = function(inp)       #create function mmode
 {
   tbl = tabulate(inp)       #arrange inp to tabular from
   maxF = max(tbl)           #calculate max tbl and assign as maxF
-  maxFC = length(which(tbl == maxF))       #À“®”π«π¢Õßmode
+  maxFC = length(which(tbl == maxF))
   out = c()                 #create NULL vecter assign as out
   
   if(maxF==1)               #display "NO MODE" While maxF=1 
@@ -12,15 +12,15 @@ mmode = function(inp)       #create function mmode
   else                      
   {
     i = 1                   #assign i as 1
-    while(i <= length(tbl)) # √È“ßloop‡æ◊ËÕ‡¢È“∂÷ß¢ÈÕ¡Ÿ≈„πtbl∑ÿ°µ—«
+    while(i <= length(tbl))
     { 
-      if(tbl[i] == maxF)    #µËÕlist out¥È«¬µ”·ÀπËß¢ÈÕ¡Ÿ≈¢Õßtbl‡¡◊ËÕtblµ—«∑’Ëi‡∑Ë“°—∫maxF
+      if(tbl[i] == maxF)
       {
-        out = append(out,c(i))#µËÕlist out¥È«¬µ”·ÀπËß¢ÈÕ¡Ÿ≈¢Õßtbl
+        out = append(out,c(i))
       }
-      i = i+1               #‡æ‘Ë¡§Ë“i 1
+      i = i+1
     }
-    cat("mode is",out,"\n") #√«¡string "mode is" ·≈– out  ‡æ◊ËÕ· ¥ßº≈
+    cat("mode is",out,"\n")
   }
   
 }
